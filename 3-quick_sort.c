e0fade7cf0a8214bde0ada19830fd5f4e387657c
octaviav <octavia7104@gmail.com>
2024-02-20 16:34:11 -0800

    sorting lists

---
+++ 3-quick_sort.c
@@ -0,0 +1,91 @@
+#include <stddef.h>
+#include "sort.h"
+
+/**
+ * quick_sort - Sort an array of integers in ascending
+ *              order using the quicksort algorithm.
+ * @array: Array of integers.
+ * @size: Size of the array.
+ *
+ * Description: Uses the Lomuto partition scheme to print
+ *              the array after each swap of two elements.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
+
+/**
+ * lomuto_sort - Recursively sorts an array of integers using quicksort
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * lomuto_partition - Partitions a subset of an array of integers using
+ *                    the lomuto partition scheme with the last element as pivot).
+ * @array: Array of integers.
+ * @size: Size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: Ending index of the subset to order.
+ *
+ * Return: Final partition index.
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, up, down;
+
+	pivot = array + right;
+	for (up = down = left; down < right; down++)
+	{
+		if (array[down] < *pivot)
+		{
+			if (up < down)
+			{
+				int_swap(array + down, array + up);
+				print_array(array, size);
+			}
+			up++;
+		}
+	}
+
+	if (array[up] > *pivot)
+	{
+		int_swap(array + up, pivot);
+		print_array(array, size);
+	}
+
+	return (up);
+}
+
+/**
+ * int_swap - Swaps two integers in an array.
+ * @x: First integer to swap.
+ * @y: Second integer to swap.
+ */
+void int_swap(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
